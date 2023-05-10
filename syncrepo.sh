7ab5bd67dd044d5f4b4d0781c708367498c8648d
Omar McIver <omar.mciver@mode3cloud.com>
2023-05-10 15:56:26 -0500

    Updated to support SSH

--- syncrepo.sh
+++ syncrepo.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
 git config --global credential.useHttpPath true
+git config --global core.sshCommand 'ssh -o StrictHostKeyChecking=no'
 
-echo "Creating empty folder for $1..."
-rm -rf $1
-mkdir $1
-cd $1 || { exit 1; }
+## $1 = Repo name
+## $2 = Branch name
+## $3 = Origin1|SSH Private Key base 64 encoded (if applicable)
+## $4 = Origin2|SSH Private Key base 64 encoded (if applicable)
+reponame="$1"
+branchname="$2"
+origin1="$3"
+origin2="$4"
 
-echo "Cloning $1 from $3..."
-git clone $3 . || { exit 1; }
+echo "Creating empty folder for $reponame..."
+rm -rf $reponame
+mkdir $reponame
+cd $reponame || { exit 1; }
 
-echo "Adding second remote as $4..."
-git remote add origin2 $4 || { exit 1; }
+if [[ "$origin1" == *"|"* ]]; then
+    echo "SSH Key for Origin 1" 
+    IFS='|'
+    read -a strarr <<< "$origin1"
+    echo "${strarr[1]}" | base64 -d > ~/.ssh/id_rsa1
+    chmod 700 ~/.ssh/id_rsa1
+    cat ~/.ssh/id_rsa1
+    ssh-add ~/.ssh/id_rsa1
+    origin1="${strarr[0]}"
+fi
 
-echo "Switching to branch $2..."
-git switch $2 || { exit 1; }
+if [[ "$origin2" == *"|"* ]]; then
+    echo "SSH Key for Origin 2"
+    IFS='|'
+    read -a strarr <<< "$origin2"
+    echo "${strarr[1]}" | base64 -d > ~/.ssh/id_rsa2
+    chmod 700 ~/.ssh/id_rsa2
+    cat ~/.ssh/id_rsa2
+    ssh-add ~/.ssh/id_rsa1
+    origin2="${strarr[0]}"
+fi
 
-echo "Pull $2 from $3..."
-git pull origin $2 || { exit 1; }
+echo "Cloning $reponame from $origin1..."
+git clone $origin1 . || { exit 1; }
 
-echo "Pull $2 from $4..."
-git pull origin2 $2 || { exit 1; }
+echo "Adding second remote as $origin2..."
+git remote add origin2 $origin2 || { exit 1; }
 
-echo "Push $2 from $3..."
-git push origin $2 || { exit 1; }
+echo "Switching to branch $branchname..."
+git switch $branchname || { exit 1; }
 
-echo "Push $2 from $4..."
-git push origin2 $2 || { exit 1; }
+echo "Pull $branchname from $origin1..."
+git pull origin $branchname || { exit 1; }
+
+echo "Pull $branchname from $origin2..."
+git pull origin2 $branchname || { exit 1; }
+
+echo "Push $branchname from $origin1..."
+git push origin $branchname || { exit 1; }
+
+echo "Push $branchname from $origin2..."
+git push origin2 $branchname || { exit 1; }
