87e5fd91e6f0e211a8c2bacfc729318bee43e83b
Omar McIver <omar.mciver@mode3cloud.com>
2023-05-11 22:16:27 -0500

    The same image file is needed otherwise devops is going to associate the key with one and then not allow it for another.

---
+++ generate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ssh-keygen -t rsa -b 4096
+public=`cat ~/.ssh/id_rsa.pub`
+private=`cat ~/.ssh/id_rsa | base64 | tr -d '\n'`
+
+echo "Register this public key with DevOps...."
+echo ""
+echo ""
+echo ""
+echo $public
+echo ""
+echo ""
+echo ""
+
+echo "Use this enoded private key with the container...."
+echo ""
+echo ""
+echo ""
+echo $private
+echo ""
+echo ""
+echo ""
+
+echo "Remember that this SSH will probably only work from one host and then DevOps will reject any usage from another host."
